f3a11405079ae0043f27c6e830d08643c9133490
tejas101k <tejassingh649@rediffmail.com>
2025-09-25 22:00:58 +0000

    Minor performance improvements

--- assets/js/templating.js
+++ assets/js/templating.js
@@ -51,11 +51,8 @@ function initializeHeaderInteractions() {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
             const target = document.querySelector(this.getAttribute('href'));
-            if (target) {
-                window.scrollTo({
-                    top: target.offsetTop - 80,
-                    behavior: 'smooth'
-                });
+            if (target && typeof target.scrollIntoView === 'function') {
+                target.scrollIntoView({ behavior: 'smooth', block: 'start' });
                 if (links) links.classList.remove('active');
             }
         });
